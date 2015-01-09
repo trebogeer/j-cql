83ff5e00b52e97eb228a62f06d4d7b2bdbc50f8d
dimav <trebogeer@gmail.com>
2015-01-08 17:23:12 -0800

    it - added schema creation and java code gen/package to jar steps

--- jcql/src/test/java/com/trebogeer/jcql/it/SmokeIT.java
+++ jcql/src/test/java/com/trebogeer/jcql/it/SmokeIT.java
@@ -13,19 +13,19 @@ import org.junit.Test;
 public class SmokeIT {
 
     static int port = Integer.getInteger("ntPort", 9042);
-    static String key_space = "test_ks";
+    static String key_space = System.getProperty("testks", "jcql");
 
     @Test
     public void test() throws Exception {
-        try{
-        Cluster c = Cluster.builder().addContactPoint("localhost").withPort(port).build();
-        Session s = c.connect();
-        KeyspaceMetadata r = s.getCluster().getMetadata().getKeyspace(key_space);
+        try {
+            Cluster c = Cluster.builder().addContactPoint("localhost").withPort(port).build();
+            Session s = c.connect();
+            KeyspaceMetadata r = s.getCluster().getMetadata().getKeyspace(key_space);
 
-        System.out.println(r.exportAsString());
+            System.out.println(r.exportAsString());
             s.close();
             c.close();
-        }catch (Exception t){
+        } catch (Exception t) {
             t.printStackTrace();
             throw t;
         }
