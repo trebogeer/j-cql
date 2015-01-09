e0d6fbfbaae35f3b2a26c7621c86054fafecbd2f
dimav <trebogeer@gmail.com>
2015-01-08 16:25:25 -0800

    casandra-maven-plugin - works with some fixes and custom build

---
+++ jcql/src/test/java/com/trebogeer/jcql/it/SmokeIT.java
@@ -0,0 +1,34 @@
+package com.trebogeer.jcql.it;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.KeyspaceMetadata;
+import com.datastax.driver.core.Session;
+import org.junit.Test;
+
+/**
+ * @author dimav
+ *         Date: 1/8/15
+ *         Time: 3:04 PM
+ */
+public class SmokeIT {
+
+    static int port = Integer.getInteger("ntPort", 9042);
+    static String key_space = "test_ks";
+
+    @Test
+    public void test() throws Exception {
+        try{
+        Cluster c = Cluster.builder().addContactPoint("localhost").withPort(port).build();
+        Session s = c.connect();
+        KeyspaceMetadata r = s.getCluster().getMetadata().getKeyspace(key_space);
+
+        System.out.println(r.exportAsString());
+            s.close();
+            c.close();
+        }catch (Exception t){
+            t.printStackTrace();
+            throw t;
+        }
+    }
+
+}
