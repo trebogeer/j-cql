737dc443b9e718593c4ffb08a7f04d4c980f9400
dimav <trebogeer@gmail.com>
2015-01-21 14:47:38 -0800

    it units, compile and run

--- jcql/src/it-gen/java/com/trebogeer/jcql/it/Smoke0IT.java
+++ jcql/src/it-gen/java/com/trebogeer/jcql/it/Smoke0IT.java
@@ -3,7 +3,6 @@ package com.trebogeer.jcql.it;
 import com.datastax.driver.core.BatchStatement;
 import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ColumnDefinitions;
 import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
@@ -128,32 +127,19 @@ public class Smoke0IT {
             PreparedStatement ps = s.prepare("INSERT INTO jcql.accounts (email, map2test, settest) " +
                     "VALUES (?,?,?)");
 
-            HashSet<String> testset = new HashSet<>();
-            testset.add("12345");
-            testset.add("536455");
-
             for (int i = 0; i < 100; i++) {
-                Map<Integer, UDTValue> test = new HashMap<>();
-                Phone ph = new Phone();
-                ph.setAlias("fodera bass" + i);
-                ph.setNumber("6503564739" + i);
-                UserType phoneUDT = s.getCluster().getMetadata().getKeyspace(key_space).getUserType("phone");
-                UDTValue uv = phoneUDT.newValue();
-                uv.setString("alias", ph.getAlias());
-                uv.setString("number", ph.getNumber());
-                test.put(32, uv);
-
-                batch.add(ps.bind("victor@wooten.com" + i, test, testset));
+                Accounts a = getTestAccount("victor@wooten@org" + i);
+                BoundStatement bs = new BoundStatement(ps);
+                Accounts.bind().bind(a, bs, s);
+                batch.add(bs);
                 if (i % 65534 == 0) {
                     ResultSet rs = s.execute(batch);
-                    //System.out.println(rs);
                     batch = new BatchStatement();
                 }
             }
 
             ResultSet rs = s.execute(batch);
 
-            //System.out.println(rs);
 
             rs = s.execute("SELECT * FROM jcql.accounts");
             List<Accounts> res = new LinkedList<>();
@@ -179,41 +165,8 @@ public class Smoke0IT {
                 Session s = c.connect(key_space)
         ) {
 
-            Phone p = new Phone();
-            p.setAlias("home");
-            p.setNumber("650-333-6666");
-
-            Address addr = new Address();
-            addr.setCity("San-Francisco");
-            List<Integer> is = new LinkedList<>();
-            is.add(1);
-            is.add(7);
-            addr.setList1test(is);
-            Map<Phone, String> map = new HashMap<>();
-            map.put(p, "don't call");
-            addr.setMap1test(map);
-            Map<Integer, String> m1 = new HashMap<>();
-            m1.put(4, "hello");
-            addr.setMap2test(m1);
-            Map<Integer, Phone> m3 = new HashMap<>();
-            m3.put(7, p);
-            addr.setMap3test(m3);
-            Map<Phone, Phone> m4 = new HashMap<>();
-            m4.put(p, p);
-            addr.setMap4test(m4);
-            List<Phone> phones = new LinkedList<>();
-            phones.add(p);
-            addr.setPhones(phones);
-            addr.setPrimaryPhone(p);
-            Set<String> set = new HashSet<>();
-            set.add("hi");
-            addr.setSet1test(set);
-            addr.setStreet("Octavia");
-
-            Accounts account = new Accounts();
-            account.setAddr(addr);
-            account.setEmail("abccdef@asdf.gfd");
-            account.setName("Primary Account");
+            String email = "abccdef@asdf.gfd";
+            Accounts account = getTestAccount(email);
             String query = "INSERT INTO accounts (addr, email, name) VALUES (?, ?, ?)";
             PreparedStatement ps1 = s.prepare(query);
             BoundStatement bs1 = new BoundStatement(ps1);
@@ -221,16 +174,52 @@ public class Smoke0IT {
             Accounts.bind().bind(account, bs1, s);
 
             s.execute(bs1);
-
-            Row row0 = s.execute("SELECT * FROM jcql.accounts WHERE email = 'abccdef@asdf.gfd'").one();
+            PreparedStatement ps = s.prepare("SELECT * FROM jcql.accounts WHERE email = ?");
+            Row row0 = s.execute(ps.bind(email)).one();
             Accounts a0 = Accounts.mapper().map(row0);
             s.execute("TRUNCATE jcql.accounts");
-        /*System.out.println("Source");
-        System.out.println(account);
-        System.out.println("Target");
-        System.out.println(a0);*/
             Assert.assertEquals("To String comparison failed", a0.toString(), account.toString());
         }
+    }
+
+
+    private static Accounts getTestAccount(String email) {
+        Phone p = new Phone();
+        p.setAlias("home");
+        p.setNumber("650-333-6666");
+
+        Address addr = new Address();
+        addr.setCity("San-Francisco");
+        List<Integer> is = new LinkedList<>();
+        is.add(1);
+        is.add(7);
+        addr.setList1test(is);
+        Map<Phone, String> map = new HashMap<>();
+        map.put(p, "don't call");
+        addr.setMap1test(map);
+        Map<Integer, String> m1 = new HashMap<>();
+        m1.put(4, "hello");
+        addr.setMap2test(m1);
+        Map<Integer, Phone> m3 = new HashMap<>();
+        m3.put(7, p);
+        addr.setMap3test(m3);
+        Map<Phone, Phone> m4 = new HashMap<>();
+        m4.put(p, p);
+        addr.setMap4test(m4);
+        List<Phone> phones = new LinkedList<>();
+        phones.add(p);
+        addr.setPhones(phones);
+        addr.setPrimaryPhone(p);
+        Set<String> set = new HashSet<>();
+        set.add("hi");
+        addr.setSet1test(set);
+        addr.setStreet("Octavia");
+
+        Accounts account = new Accounts();
+        account.setAddr(addr);
+        account.setEmail(email);
+        account.setName("Primary Account");
+        return account;
     }
 
 }
